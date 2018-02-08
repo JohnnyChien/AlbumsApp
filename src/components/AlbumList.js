f3136464e6d6b8311320672222fba518128549b2
Johnny <emailjchien@gmail.com>
2018-02-08 09:36:18 -0800

    Completed API call to get album data.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
 
+  renderAlbums() {
+    return this.state.albums.map(
+      album => <Text key={album.title}>{album.title}</Text>);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 export default AlbumList;
