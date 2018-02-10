0e03512b13cfa12c87a5bc2ba164db780c1f8b70
Johnny <emailjchien@gmail.com>
2018-02-10 01:31:18 -0800

    Added art work image and style. Not scrollable yet.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,12 +4,13 @@ import Card from './Card'
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const {
     headerContentStyle,
     thumbnailStyle,
     thumbnailContainerStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
   return (
     <Card>
@@ -24,6 +25,12 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}> { title } </Text>
           <Text> { artist } </Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
       </CardSection>
     </Card>);
 };
@@ -45,6 +52,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
